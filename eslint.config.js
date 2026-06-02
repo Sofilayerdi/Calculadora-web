563231ff233160c618c84e39bd5922f9e6c160f9
Sofilayerdi <sofilayerdi@gmail.com>
2026-06-01 22:33:15 -0600

    stories y lint

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,19 @@
+import storybook from "eslint-plugin-storybook"
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
+import stylistic from '@stylistic/eslint-plugin'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'storybook-static']),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ['**/*.{ts,tsx,js,jsx}'],
+    plugins: {
+      '@stylistic': stylistic,
+    },
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
@@ -18,5 +23,10 @@ export default defineConfig([
     languageOptions: {
       globals: globals.browser,
     },
+    rules: {
+      '@stylistic/semi': ['error', 'never'],
+      'max-len': ['error', { code: 120 }],
+    },
   },
+  ...storybook.configs["flat/recommended"],
 ])
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-// src/App.tsx
 import './App.css'
 import Calculator from './components/Calculator/Calculator'
 
