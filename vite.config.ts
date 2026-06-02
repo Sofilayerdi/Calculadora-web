08ebdf455e8141df9f96e569cfe1811ee5534fbd
Sofilayerdi <sofilayerdi@gmail.com>
2026-06-01 23:01:47 -0600

    fix build

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+
 // https://vite.dev/config/
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
@@ -12,6 +13,7 @@ const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(file
 // More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   plugins: [react()],
+  base: '/Calculadora-web/',
   test: {
     projects: [{
       extends: true,
